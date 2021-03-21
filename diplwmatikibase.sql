8de590d3cb18583c7ec3dee1ca96ae91165f73b4
Dimitris  Motsios <dmotsios@terracom.gr>
2021-03-21 23:56:51 +0200

    new sql

--- diplwmatikibase.sql
+++ diplwmatikibase.sql
@@ -17,20 +17,46 @@ DROP DATABASE IF EXISTS `diplwmatiki`;
 CREATE DATABASE IF NOT EXISTS `diplwmatiki` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `diplwmatiki`;
 
+-- Dumping structure for table diplwmatiki.books
+DROP TABLE IF EXISTS `books`;
+CREATE TABLE IF NOT EXISTS `books` (
+  `idbook` int(12) NOT NULL,
+  `title` varchar(22) COLLATE utf8_bin NOT NULL,
+  `releasedDate` datetime NOT NULL,
+  `idwriter` int(12) DEFAULT NULL,
+  PRIMARY KEY (`idbook`),
+  KEY `idwriter` (`idwriter`),
+  CONSTRAINT `idwriter` FOREIGN KEY (`idwriter`) REFERENCES `writers` (`idwriter`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Dumping data for table diplwmatiki.books: ~0 rows (approximately)
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+REPLACE INTO `books` (`idbook`, `title`, `releasedDate`, `idwriter`) VALUES
+	(1, 'La la land', '2020-03-03 00:00:00', 2),
+	(2, 'Tommorow', '1994-03-03 00:00:00', NULL),
+	(3, 'The day after Tommorow', '1992-12-12 00:00:00', 1),
+	(4, 'Friends With Benefits', '2001-06-12 00:00:00', 6),
+	(5, 'Alone', '1993-11-10 00:00:00', 6),
+	(6, 'The lords of the ring', '1998-06-03 00:00:00', 7),
+	(7, 'Warm Up', '1994-03-01 00:00:00', 6);
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+
 -- Dumping structure for table diplwmatiki.excersice_tables
 DROP TABLE IF EXISTS `excersice_tables`;
 CREATE TABLE IF NOT EXISTS `excersice_tables` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `table_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=170 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=174 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Dumping data for table diplwmatiki.excersice_tables: ~2 rows (approximately)
+-- Dumping data for table diplwmatiki.excersice_tables: ~3 rows (approximately)
 /*!40000 ALTER TABLE `excersice_tables` DISABLE KEYS */;
 REPLACE INTO `excersice_tables` (`id`, `table_name`) VALUES
 	(116, 'asdsad'),
 	(166, 'players'),
-	(167, 'location');
+	(167, 'location'),
+	(172, 'writers'),
+	(173, 'books');
 /*!40000 ALTER TABLE `excersice_tables` ENABLE KEYS */;
 
 -- Dumping structure for table diplwmatiki.fill_fields_questions
@@ -91,7 +117,7 @@ CREATE TABLE IF NOT EXISTS `location` (
   CONSTRAINT `player_id` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Dumping data for table diplwmatiki.location: ~0 rows (approximately)
+-- Dumping data for table diplwmatiki.location: ~8 rows (approximately)
 /*!40000 ALTER TABLE `location` DISABLE KEYS */;
 REPLACE INTO `location` (`idlocation`, `address`, `city`, `player_id`) VALUES
 	(1, 'antwni samaraki 16', 'ioannina', 4),
@@ -116,7 +142,7 @@ CREATE TABLE IF NOT EXISTS `players` (
   KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Dumping data for table diplwmatiki.players: ~0 rows (approximately)
+-- Dumping data for table diplwmatiki.players: ~11 rows (approximately)
 /*!40000 ALTER TABLE `players` DISABLE KEYS */;
 REPLACE INTO `players` (`id`, `name`, `surname`, `score`) VALUES
 	(1, 'dimitris', 'motsios', '100'),
@@ -147,9 +173,9 @@ CREATE TABLE IF NOT EXISTS `score_table` (
   PRIMARY KEY (`id`),
   KEY `student_id_idx` (`student_id`),
   CONSTRAINT `student_id` FOREIGN KEY (`student_id`) REFERENCES `user_table` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=200 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=204 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Dumping data for table diplwmatiki.score_table: ~46 rows (approximately)
+-- Dumping data for table diplwmatiki.score_table: ~27 rows (approximately)
 /*!40000 ALTER TABLE `score_table` DISABLE KEYS */;
 REPLACE INTO `score_table` (`id`, `student_id`, `score`, `time`, `category`, `difficulty`, `created_at`, `updated_at`) VALUES
 	(20, 5, 65, '00:01:22', '15 Questions', 'hard', '2020-12-08 17:24:31', '2020-12-08 17:24:31'),
@@ -178,7 +204,11 @@ REPLACE INTO `score_table` (`id`, `student_id`, `score`, `time`, `category`, `di
 	(73, 22, 72, '00:00:20', '15 Questions', 'hard', '2021-01-12 19:07:19', '2021-01-12 19:07:19'),
 	(74, 22, 88, '00:00:30', '25 Questions', 'hard', '2021-01-12 19:08:13', '2021-01-12 19:08:13'),
 	(75, 23, 82, '00:00:18', '15 Questions', 'hard', '2021-01-12 19:09:48', '2021-01-12 19:09:48'),
-	(76, 23, 45, '00:00:26', '25 Questions', 'hard', '2021-01-12 19:10:34', '2021-01-12 19:10:34');
+	(76, 23, 45, '00:00:26', '25 Questions', 'hard', '2021-01-12 19:10:34', '2021-01-12 19:10:34'),
+	(200, 26, 20, '00:00:19', '15 Questions', 'easy', '2021-03-20 01:11:20', '2021-03-20 01:11:20'),
+	(201, 26, 20, '00:00:19', '15 Questions', 'easy', '2021-03-20 01:11:50', '2021-03-20 01:11:50'),
+	(202, 26, 29, '00:00:17', '15 Questions', 'easy', '2021-03-20 01:12:23', '2021-03-20 01:12:23'),
+	(203, 26, 72, '00:00:35', '25 Questions', 'easy', '2021-03-20 01:13:14', '2021-03-20 01:13:14');
 /*!40000 ALTER TABLE `score_table` ENABLE KEYS */;
 
 -- Dumping structure for table diplwmatiki.sql_questions
@@ -271,9 +301,9 @@ CREATE TABLE IF NOT EXISTS `sql_random_queries` (
   PRIMARY KEY (`id`),
   KEY `table_id_idx` (`table_id`),
   CONSTRAINT `table_id` FOREIGN KEY (`table_id`) REFERENCES `excersice_tables` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=739 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=769 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Dumping data for table diplwmatiki.sql_random_queries: ~23 rows (approximately)
+-- Dumping data for table diplwmatiki.sql_random_queries: ~30 rows (approximately)
 /*!40000 ALTER TABLE `sql_random_queries` DISABLE KEYS */;
 REPLACE INTO `sql_random_queries` (`id`, `sql_query`, `hideWord`, `table_id`) VALUES
 	(709, 'SELECT * FROM players', '*,players', 166),
@@ -305,7 +335,37 @@ REPLACE INTO `sql_random_queries` (`id`, `sql_query`, `hideWord`, `table_id`) VA
 	(735, 'SELECT id FROM players UNION SELECT player_id FROM location', 'UNION', 167),
 	(736, 'SELECT surname FROM players WHERE id = ANY ( SELECT player_id FROM location WHERE player_id>3 )', 'ANY', 167),
 	(737, 'SELECT id FROM players WHERE id = ALL ( SELECT player_id FROM location WHERE player_id=3 )', 'ALL', 167),
-	(738, 'SELECT score FROM players WHERE id IN ( SELECT player_id FROM location WHERE player_id>2 )', 'IN', 167);
+	(738, 'SELECT score FROM players WHERE id IN ( SELECT player_id FROM location WHERE player_id>2 )', 'IN', 167),
+	(739, 'SELECT * FROM writers', '*,writers', 172),
+	(740, 'SELECT idwriter, name FROM writers', 'idwriter,name', 172),
+	(741, 'SELECT DISTINCT surname FROM writers', 'DISTINCT', 172),
+	(742, 'SELECT COUNT(age) FROM writers', 'COUNT,FROM', 172),
+	(743, 'SELECT COUNT(DISTINCT surname) FROM writers', 'DISTINCT', 172),
+	(744, 'SELECT * FROM writers WHERE idwriter=7', 'WHERE,idwriter', 172),
+	(745, 'SELECT * FROM writers WHERE idwriter IN ("1","4")', 'IN', 172),
+	(746, 'SELECT * FROM writers WHERE idwriter BETWEEN 2 AND 7', 'BETWEEN,AND', 172),
+	(747, 'SELECT * FROM writers ORDER BY age', 'ORDER BY', 172),
+	(748, 'SELECT * FROM writers ORDER BY surname DESC', 'ORDER BY,DESC', 172),
+	(749, 'SELECT * FROM writers ORDER BY surname ASC, age DESC', 'ORDER BY,ASC,DESC', 172),
+	(750, 'SELECT * FROM writers LIMIT 3', 'LIMIT', 172),
+	(751, 'SELECT MIN(idwriter) FROM writers', 'MIN', 172),
+	(752, 'SELECT MAX(idwriter) FROM writers', 'MAX', 172),
+	(753, 'SELECT COUNT(surname), age FROM writers GROUP BY age', 'COUNT,GROUP BY', 172),
+	(754, 'SELECT COUNT(idwriter), name FROM writers GROUP BY name ORDER BY COUNT(idwriter) DESC', 'ORDER BY,DESC', 172),
+	(755, 'SELECT COUNT(surname) FROM writers GROUP BY age HAVING COUNT(surname) >3', 'GROUP BY,HAVING COUNT', 172),
+	(756, 'SELECT SUM(idwriter) FROM writers', 'SUM', 172),
+	(757, 'SELECT AVG(idwriter) FROM writers', 'AVG', 172),
+	(758, 'SELECT * FROM writers WHERE age IS NULL', 'IS NULL', 172),
+	(759, 'SELECT writers.idwriter, writers.name, books.title, books.releasedDate FROM writers, books WHERE writers.idwriter=books.idwriter', 'writers,idwriter,=', 173),
+	(760, 'SELECT writers.idwriter, writers.name, books.title FROM writers INNER JOIN books ON writers.idwriter=books.idwriter', 'INNER,JOIN', 173),
+	(761, 'SELECT writers.name, books.idwriter FROM writers LEFT JOIN books ON writers.idwriter=books.idwriter ORDER BY writers.name', 'LEFT,ORDER BY', 173),
+	(762, 'SELECT writers.idwriter, books.title, books.releasedDate FROM writers RIGHT JOIN books ON writers.idwriter=books.idwriter ORDER BY writers.idwriter DESC', 'RIGHT,DESC', 173),
+	(763, 'SELECT writers.idwriter, writers.name, books.title FROM writers INNER JOIN books ON writers.idwriter=books.idwriter WHERE writers.idwriter>3', 'WHERE,>', 173),
+	(764, 'SELECT * FROM books CROSS JOIN writers', 'CROSS', 173),
+	(765, 'SELECT idwriter FROM writers UNION SELECT idwriter FROM books', 'UNION', 173),
+	(766, 'SELECT age FROM writers WHERE idwriter = ANY ( SELECT idwriter FROM books WHERE idwriter>3 )', 'ANY', 173),
+	(767, 'SELECT idwriter FROM writers WHERE idwriter = ALL ( SELECT idwriter FROM books WHERE idwriter=3 )', 'ALL', 173),
+	(768, 'SELECT surname FROM writers WHERE idwriter IN ( SELECT idwriter FROM books WHERE idwriter>2 )', 'IN', 173);
 /*!40000 ALTER TABLE `sql_random_queries` ENABLE KEYS */;
 
 -- Dumping structure for table diplwmatiki.success_rate
@@ -321,9 +381,9 @@ CREATE TABLE IF NOT EXISTS `success_rate` (
   PRIMARY KEY (`id`),
   KEY `id_student_idx` (`id_student`),
   CONSTRAINT `id_student` FOREIGN KEY (`id_student`) REFERENCES `user_table` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=50 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
--- Dumping data for table diplwmatiki.success_rate: ~32 rows (approximately)
+-- Dumping data for table diplwmatiki.success_rate: ~8 rows (approximately)
 /*!40000 ALTER TABLE `success_rate` DISABLE KEYS */;
 REPLACE INTO `success_rate` (`id`, `id_student`, `rate`, `time`, `table_name`, `created_at`, `updated_at`) VALUES
 	(9, 5, 'Σωστές 1 στις 5', '00:00:20', 'players', '2021-02-22 16:30:52', '2021-02-22 16:30:52'),
@@ -333,7 +393,8 @@ REPLACE INTO `success_rate` (`id`, `id_student`, `rate`, `time`, `table_name`, `
 	(45, 5, 'Σωστές 13 στις 20', '00:02:13', 'table1', '2021-03-08 18:09:57', '2021-03-08 18:09:57'),
 	(46, 5, 'Σωστές 2 στις 10', '00:01:45', 'table11', '2021-03-08 18:11:59', '2021-03-08 18:11:59'),
 	(47, 5, 'Σωστές 0 στις 10', '00:01:21', 'test33', '2021-03-16 21:50:48', '2021-03-16 21:50:48'),
-	(48, 5, 'Σωστές 8 στις 10', '00:01:51', 'location', '2021-03-18 21:58:29', '2021-03-18 21:58:29');
+	(48, 5, 'Σωστές 8 στις 10', '00:01:51', 'location', '2021-03-18 21:58:29', '2021-03-18 21:58:29'),
+	(49, 26, 'Σωστές 3 στις 10', '00:00:54', 'location', '2021-03-20 01:14:49', '2021-03-20 01:14:49');
 /*!40000 ALTER TABLE `success_rate` ENABLE KEYS */;
 
 -- Dumping structure for table diplwmatiki.user_table
@@ -351,7 +412,7 @@ CREATE TABLE IF NOT EXISTS `user_table` (
   `verification_code` varchar(500) CHARACTER SET utf8 COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username_UNIQUE` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- Dumping data for table diplwmatiki.user_table: ~7 rows (approximately)
 /*!40000 ALTER TABLE `user_table` DISABLE KEYS */;
@@ -362,8 +423,33 @@ REPLACE INTO `user_table` (`id`, `first_name`, `last_name`, `sex`, `phone`, `ema
 	(16, 'dionisis', 'papakwstas', 'male', '6936547892', 'dionisis@gmail.com', 'dioni', '$2a$10$AJQJhxAZVVTDbJwU75qGm.BW9LDld1qvaGy/n7dkU9Jw2j9jDSMbq', 'student', NULL),
 	(22, 'aleksis', 'koumasis', 'male', '6932156941', 'aleksis@gmail.com', 'aleksis', '$2a$10$4kT52oOxyPwuXXU6LCSMY.OkC8uZOKrZXX2NXIdb7/6/N5GQuO9u.', 'student', NULL),
 	(23, 'georgia', 'kali', 'female', '6932569515', 'georgia@gmail.com', 'georgia', '$2a$10$jGKcviqvwK3RqFy/kmUvfe3ofLmHU2HiYQw1G0uWjh.8ItYqZB7G.', 'student', NULL),
-	(24, 'Iliana', 'papanikou', 'female', '6945611212', 'Iliana@gmail.com', 'iliana', '$2a$10$kaBhe10ytWP.RhUxuYwgbe3x1cSG6/R0TUe.MsoMZf77BPAHwJyyi', 'student', NULL);
+	(24, 'Iliana', 'papanikou', 'female', '6945611212', 'Iliana@gmail.com', 'iliana', '$2a$10$kaBhe10ytWP.RhUxuYwgbe3x1cSG6/R0TUe.MsoMZf77BPAHwJyyi', 'student', NULL),
+	(26, 'George', 'Andreou', 'male', '6354567891', 'George', 'giorgos93', '$2a$10$m9YdINIylv4V6Rs0NSim3uJJhgGlPb89rRfbMd9SsRXFZU9T14xKa', 'student', NULL);
 /*!40000 ALTER TABLE `user_table` ENABLE KEYS */;
+
+-- Dumping structure for table diplwmatiki.writers
+DROP TABLE IF EXISTS `writers`;
+CREATE TABLE IF NOT EXISTS `writers` (
+  `idwriter` int(12) NOT NULL,
+  `name` varchar(22) COLLATE utf8_bin NOT NULL,
+  `surname` varchar(22) COLLATE utf8_bin NOT NULL,
+  `age` varchar(22) COLLATE utf8_bin DEFAULT NULL,
+  PRIMARY KEY (`idwriter`),
+  KEY `idwriter` (`idwriter`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- Dumping data for table diplwmatiki.writers: ~0 rows (approximately)
+/*!40000 ALTER TABLE `writers` DISABLE KEYS */;
+REPLACE INTO `writers` (`idwriter`, `name`, `surname`, `age`) VALUES
+	(1, 'Dimitris', 'Motsios', '22'),
+	(2, 'Georgia', 'Pappandreou', '36'),
+	(3, 'Kostas', 'Mixas', '44'),
+	(4, 'Ilianna', 'Pappagewrgiou', '42'),
+	(5, 'Eleonora', 'Meleti', '36'),
+	(6, 'Xristos', 'Sarris', '36'),
+	(7, 'Kostas', 'Pappas', '23'),
+	(8, 'Panos', 'Panou', '36');
+/*!40000 ALTER TABLE `writers` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
