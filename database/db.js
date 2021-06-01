751d632dad8e15ca6bdd3f3b3e0502f7b7668242
Dimitris  Motsios <dmotsios@terracom.gr>
2021-06-01 20:58:41 +0300

    change credentials

--- database/db.js
+++ database/db.js
@@ -1,7 +1,27 @@
-const Sequelize = require('sequelize')
+/*const Sequelize = require('sequelize')
 const db = {}
 const sequelize= new Sequelize('diplwmatiki','root','m19941994', {
     host :'localhost',
+    dialect:'mysql',
+    timezone: "+02:00",
+    operatorsAliases: false,
+    
+    pool:{
+        max:5,
+        min:0,
+        acquire:30000,
+        idle:10000
+    }
+})
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+module.exports = db*/
+
+const Sequelize = require('sequelize')
+const db = {}
+const sequelize= new Sequelize('heroku_f7049074a296508','ba1982a523b6c5','cf4138f6', {
+    host :'us-cdbr-east-04.cleardb.com',
     dialect:'mysql',
     timezone: "+02:00",
     operatorsAliases: false,
